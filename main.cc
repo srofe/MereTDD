510cff11d261c6e56f8fb5a5094c859ca69cc028
Simon Rofe <simon.rofe@ieee.org>
2022-12-01 09:42:37 +1000

    Move macro definition to a separate file

--- main.cc
+++ main.cc
@@ -1,20 +1,9 @@
+#include "Test.h"
 #include <iostream>
-#include <string_view>
 
-#define TEST class Test { \
-public: \
-    Test(std::string_view name) : mName(name), mResult(true) {} \
-    void operator() (); \
-private: \
-    std::string mName; \
-    bool mResult; \
-}; \
-Test test("testCanBeCreated"); \
-void Test::operator() ()
 TEST {
         std::cout << mName << std::endl;
 }
-
 
 int main() {
     test();
