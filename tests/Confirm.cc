851a5d4999e7aee714bedeea836f1d55945a592c
Simon Rofe <simon.rofe@ieee.org>
2022-12-05 11:23:22 +1000

    Add tests for long long type

--- tests/Confirm.cc
+++ tests/Confirm.cc
@@ -12,6 +12,10 @@ long multiplyBy2(long value) {
     return value * 2L;
 }
 
+long long multiplyBy2(long long value) {
+    return value * 2LL;
+}
+
 TEST("Test will pass without any confirms") {
 }
 
@@ -41,6 +45,15 @@ TEST("Test long confirms") {
     CONFIRM(-2L, result);
 }
 
+TEST("Test long long confirms") {
+    long long result = multiplyBy2(0LL);
+    CONFIRM(0LL, result);
+    result = multiplyBy2(10'000'000'000LL);
+    CONFIRM(20'000'000'000, result);
+    result = multiplyBy2(-10'000'000'000LL);
+    CONFIRM(-20'000'000'000, result);
+}
+
 TEST("Test bool confirm failure") {
     std::string reason = "    Expected: true";
     setExpectedFailureReason(reason);
@@ -62,4 +75,12 @@ TEST("Test long confirm failure") {
     setExpectedFailureReason(reason);
     long result = multiplyBy2(1L);
     CONFIRM(0L, result);
+}
+
+TEST("Test long long confirm failure") {
+    std::string reason = "    Expected: 10000000000\n";
+    reason += "    Actual  : 20000000000";
+    setExpectedFailureReason(reason);
+    long long result = multiplyBy2(10'000'000'000LL);
+    CONFIRM(10'000'000'000LL, result);
 }
