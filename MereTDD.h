a39f4ae8c7cbcec1669599bab662c9442caa93e6
Simon Rofe <simon.rofe@ieee.org>
2022-12-11 20:53:06 +1000

    Refactor to move TestSuite class to its own file

--- MereTDD.h
+++ MereTDD.h
@@ -11,25 +11,10 @@
 #include "MissingException.h"
 #include "TestBase.h"
 #include "Test.h"
+#include "TestSuite.h"
 #include "confirm.h"
 
 namespace MereTDD {
-    class Test;
-    class TestSuite;
-
-    inline std::map<std::string, std::vector<TestSuite *>>& getTestSuites() {
-        static std::map<std::string, std::vector<TestSuite *>> suites;
-        return suites;
-    }
-
-    inline void addTestSuite(std::string_view suiteName, TestSuite* suite) {
-        std::string name(suiteName);
-        if (not getTestSuites().contains(name)) {
-            getTestSuites().try_emplace(name, std::vector<TestSuite*>());
-        }
-        getTestSuites()[name].push_back(suite);
-    }
-
     template <typename ExceptionT>
     class TestEx : public Test {
     public:
@@ -46,16 +31,6 @@ namespace MereTDD {
 
     private:
         std::string mExceptionName;
-    };
-
-    class TestSuite : public TestBase {
-    public:
-        TestSuite(std::string_view name, std::string_view suiteName) : TestBase(name, suiteName) {
-            addTestSuite(suiteName, this);
-        }
-
-        virtual void suiteSetup() = 0;
-        virtual void suiteTeardown() = 0;
     };
 
     inline void runTest(std::ostream& output, Test* test, int& numPassed, int& numFailed, int& numMissedFailed) {
---
+++ TestSuite.h
@@ -0,0 +1,33 @@
+#ifndef MERETDD_TESTSUITE_H
+#define MERETDD_TESTSUITE_H
+
+
+
+namespace MereTDD {
+    class TestSuite;
+
+    inline std::map<std::string, std::vector<TestSuite *>>& getTestSuites() {
+        static std::map<std::string, std::vector<TestSuite *>> suites;
+        return suites;
+    }
+
+    inline void addTestSuite(std::string_view suiteName, TestSuite* suite) {
+        std::string name(suiteName);
+        if (not getTestSuites().contains(name)) {
+            getTestSuites().try_emplace(name, std::vector<TestSuite*>());
+        }
+        getTestSuites()[name].push_back(suite);
+    }
+
+    class TestSuite : public TestBase {
+    public:
+        TestSuite(std::string_view name, std::string_view suiteName) : TestBase(name, suiteName) {
+            addTestSuite(suiteName, this);
+        }
+
+        virtual void suiteSetup() = 0;
+        virtual void suiteTeardown() = 0;
+    };
+}
+
+#endif //MERETDD_TESTSUITE_H
